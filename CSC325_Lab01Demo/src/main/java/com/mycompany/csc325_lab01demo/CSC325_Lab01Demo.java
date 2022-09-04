404d1297f65c2fdc9787c15558e067a13295c5c2
Christopher Canenguez <canenguez.chris@gmail.com>
2022-09-04 13:27:28 -0400

    Saving a working Java file.

---
+++ CSC325_Lab01Demo/src/main/java/com/mycompany/csc325_lab01demo/CSC325_Lab01Demo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.csc325_lab01demo;
+
+/**
+ *
+ * @author chriscanenguez
+ */
+public class CSC325_Lab01Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Java Programming");
+    }
+}
